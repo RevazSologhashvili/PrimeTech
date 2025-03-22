f14ef94026f7338cfe9755ee543cfcb76836f318
RevazSologhashvili <revaz.sologhashvili@gmail.com>
2025-03-22 14:02:15 +0400

    update-v4 - fixed build errors - removed prisma

--- prisma/migrations/20250209155452_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `imageHref` to the `Service` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Service" ADD COLUMN     "imageHref" TEXT NOT NULL;
