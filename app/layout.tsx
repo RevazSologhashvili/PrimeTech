4f77da24b0914166cacd79c6bc0080a8962b1e4c
RevazSologhashvili <revaz.sologhashvili@gmail.com>
2025-03-30 16:01:51 +0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ka">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased` + " min-h-screen bg-main"}
       >
